6342e198f00743af6832849c0c099ef1b6d7d35a
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2022-10-23 21:15:47 +0200

    more formatting

--- src/AST.hpp
+++ src/AST.hpp
@@ -1,38 +1,38 @@
 #pragma once
 
-#include <string>
-#include <memory>
-#include <vector>
-
 #include "llvm/IR/Constants.h"
 
+#include <memory>
+#include <string>
+#include <vector>
+
 namespace AST {
-enum instructions
-{
-    add
-};
+  enum instructions { add };
 
-class Expr {
-public:
-    virtual ~Expr() {}
+  class Expr {
+  public:
+    virtual ~Expr() { }
+
     virtual llvm::Value* codegen() = 0;
-};
+  };
 
-class NumberExpr : public Expr {
+  class NumberExpr : public Expr {
     int _val;
 
-public:
-    NumberExpr(int val) : _val(val) {}
-    llvm::Value* codegen() override;
-};
+  public:
+    NumberExpr(int val) : _val(val) { }
 
-class InstructionExpr : public Expr {
+    llvm::Value* codegen() override;
+  };
+
+  class InstructionExpr : public Expr {
     int _instruction;
     std::vector<std::unique_ptr<Expr>> _args;
-public:
-    InstructionExpr(const int instruction,
-        std::vector<std::unique_ptr<Expr>> args)
-        : _instruction(instruction), _args(std::move(args)) {}
+
+  public:
+    InstructionExpr(const int instruction, std::vector<std::unique_ptr<Expr>> args) :
+        _instruction(instruction), _args(std::move(args)) { }
+
     llvm::Value* codegen() override;
-};
-}
+  };
+} // namespace AST
