e9678979a02506dd73d0db5f3a9afb55df1a1421
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2022-11-11 16:27:48 +0100

    read file

--- frame_compiler/src/main.rs
+++ frame_compiler/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
+use clap::Parser;
+#[derive(Parser)]
+pub struct Cli {
+    pub path: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let file = fs::read_to_string(&args.path).expect("Could not read file");
+    println!("{}", file)
 }
