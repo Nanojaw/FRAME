bd16ac82534857397bde0274ad610d00331aa342
= <=>
2022-12-07 13:18:40 +0100

    stuff

--- frame_compiler/src/llvm_ir_translator.rs
+++ frame_compiler/src/llvm_ir_translator.rs
@@ -1,7 +1,32 @@
 use crate::parser::ProcessedBlock;
 use llvm_ir;
 
+pub fn value_block_to_llvm_ir(
+    value_block: &ProcessedBlock::ProcessedValueBlock,
+) -> llvm_ir::translator::IrEnum {
+    match value_block.value {
+        ProcessedBlock::ValueTypes::Bool(bool) => {
+            
+        }
+
+        ProcessedBlock::ValueTypes::String(str) => todo!(),
+        ProcessedBlock::ValueTypes::Number(numType) => todo!(),
+        ProcessedBlock::ValueTypes::Variable(var) => todo!(),
+    }
+}
+
 impl ProcessedBlock::ProcessedBlock {
     pub fn to_llvm_ir(&self) -> llvm_ir::translator::IrEnum {
+        match self {
+            ProcessedBlock::ProcessedBlock::ProcessedInstrWithBody(InstructionWithBody) => todo!(),
+            ProcessedBlock::ProcessedBlock::ProcessedInstr(Instr) => todo!(),
+
+            ProcessedBlock::ProcessedBlock::ProcessedValue(valueBlock) => {
+                return value_block_to_llvm_ir(valueBlock);
+            }
+
+            ProcessedBlock::ProcessedBlock::ProcessedStructure(Structure) => todo!(),
+            ProcessedBlock::ProcessedBlock::ProcessedArray(Array) => todo!(),
+        }
     }
 }
